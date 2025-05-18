6e4081a4135706b708b7d70a1760a14ad3c92b4d
Maxim <yrev2011@gmail.com>
2025-05-18 23:50:01 +0700

    Parametrize api tests

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,65 +1,64 @@
+import pytest
 from httpx import AsyncClient
 
 
-async def test_get_trading_dates_success(api_client: AsyncClient):
-    response = await api_client.get("tradings/dates/")
+@pytest.mark.parametrize(
+    "params, status_code, length", [
+        ({}, 200, 3),
+        ({"limit": 2, "offset": 1}, 200, 2),
+        ({"limit": -10}, 422, None),
+    ]
+)
+async def test_get_trading_dates(
+        api_client: AsyncClient,
+        params: dict[str, int],
+        status_code: int,
+        length: int,
+):
+    response = await api_client.get("tradings/dates/", params=params)
+    assert response.status_code == status_code
+
+    if status_code == 200:
+        body = response.json()
+        assert len(body["days"]) == length
+
+
+@pytest.mark.parametrize(
+    "params, status_code, length", [
+        ({}, 200, 3),
+        ({"oil_id": "A100"}, 200, 1),
+    ]
+)
+async def test_get_trading_result(
+        api_client: AsyncClient,
+        params: dict[str, str],
+        status_code: int,
+        length: int
+):
+    response = await api_client.get("tradings/", params=params)
     body = response.json()
-    assert response.status_code == 200
-    assert len(body["days"]) == 3
+    assert response.status_code == status_code
+    assert len(body["trading_results"]) == length
 
 
-async def test_get_trading_dates_w_limit_and_offset(api_client: AsyncClient):
-    response = await api_client.get("tradings/dates/", params={"limit": 2, "offset": 1})
-    body = response.json()
-    assert response.status_code == 200
-    assert len(body["days"]) == 2
+@pytest.mark.parametrize(
+    "params, status_code, length", [
+        ({"start_date": "2024-08-07", "end_date": "2024-08-10"}, 200, 3),
+        ({"start_date": "2024-08-07", "end_date": "2024-08-08"}, 200, 2),
+        ({}, 422, None),
+    ]
+)
+async def test_get_trading_dynamics_success(
+        api_client: AsyncClient,
+        params: dict[str, str],
+        status_code: int,
+        length: int | None,
+):
+    response = await api_client.get("tradings/dynamics/", params=params)
+    assert response.status_code == status_code
+
+    if status_code == 200:
+        body = response.json()
+        assert len(body["trading_results"]) == length
 
 
-async def test_get_trading_dates_w_validation_error(api_client: AsyncClient):
-    response = await api_client.get("tradings/dates/", params={"limit": -10})
-    assert response.status_code == 422
-
-
-async def test_get_trading_result_success(api_client: AsyncClient):
-    response = await api_client.get("tradings/")
-    body = response.json()
-    assert response.status_code == 200
-    assert len(body["trading_results"]) == 3
-
-
-async def test_get_trading_result_w_filter(api_client: AsyncClient):
-    response = await api_client.get("tradings/", params={"oil_id": "A100"})
-    body = response.json()
-    assert response.status_code == 200
-    assert len(body["trading_results"]) == 1
-
-
-async def test_get_trading_dynamics_success(api_client: AsyncClient):
-    response = await api_client.get(
-        "tradings/dynamics/",
-        params={
-            "start_date": "2024-08-07",
-            "end_date": "2024-08-10",
-        }
-    )
-    body = response.json()
-    assert response.status_code == 200
-    assert len(body["trading_results"]) == 3
-
-
-async def test_get_trading_dynamics_w_data_filter(api_client: AsyncClient):
-    response = await api_client.get(
-        "tradings/dynamics/",
-        params={
-            "start_date": "2024-08-07",
-            "end_date": "2024-08-08",
-        }
-    )
-    body = response.json()
-    assert response.status_code == 200
-    assert len(body["trading_results"]) == 2
-
-
-async def test_get_trading_dynamics_w_validation_error(api_client: AsyncClient):
-    response = await api_client.get("tradings/dynamics/")
-    assert response.status_code == 422
