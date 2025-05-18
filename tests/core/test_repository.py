b4fef88be5b3ac80674504b8b51578c8af94102c
Maxim <yrev2011@gmail.com>
2025-05-18 23:37:08 +0700

    Parametrize repositories tests

--- tests/core/test_repository.py
+++ tests/core/test_repository.py
@@ -1,5 +1,7 @@
 import datetime
 
+import pytest
+
 from src.trading.interfaces import TradingResultRepository
 
 type StartPeriodDate = datetime.date
@@ -11,119 +13,101 @@ async def test_get_trading_date_count(trading_repository: TradingResultRepositor
     assert count == 3
 
 
-async def test_get_last_trading_date(trading_repository: TradingResultRepository):
-    trading_date_list = await trading_repository.get_last_trading_date(limit=20, offset=0)
-    assert len(trading_date_list) == 3
+@pytest.mark.parametrize(
+    "limit, offset, length", [
+        (20, 0, 3),
+        (20, 2, 1),
+    ]
+)
+async def test_get_last_trading_date(
+        trading_repository: TradingResultRepository,
+        limit: int,
+        offset: int,
+        length: int,
+):
+    trading_date_list = await trading_repository.get_last_trading_date(limit=limit, offset=offset)
+    assert len(trading_date_list) == length
 
 
-async def test_get_last_trading_date_w_pagination(trading_repository: TradingResultRepository):
-    trading_date_list = await trading_repository.get_last_trading_date(limit=20, offset=2)
-    assert len(trading_date_list) == 1
-
-
-async def get_trading_result_count_for_period(
+@pytest.mark.parametrize(
+    "filters, count", [
+        ({}, 3),
+        ({"oil_id": "A10K"}, 1),
+    ]
+)
+async def test_get_trading_result_count_for_period(
         trading_repository: TradingResultRepository,
         period: tuple[StartPeriodDate, EndPeriodDate],
+        filters: dict[str, str],
+        count: int,
 ):
     start, end = period
     trading_result_count = await trading_repository.get_trading_result_count_for_period(
-        filters={},
+        filters=filters,
         start_period_date=start,
         end_period_date=end
     )
-    assert trading_result_count == 3
+    assert trading_result_count == count
 
 
-async def test_get_trading_result_count_for_period_w_filters(
-        trading_repository: TradingResultRepository,
-        period: tuple[StartPeriodDate, EndPeriodDate],
-):
-    start, end = period
-    trading_result_count = await trading_repository.get_trading_result_count_for_period(
-        filters={"oil_id": "A10K"},
-        start_period_date=start,
-        end_period_date=end
-    )
-    assert trading_result_count == 1
-
-
+@pytest.mark.parametrize(
+    "filters, limit, offset, length", [
+        ({}, 20, 0, 3),
+        ({}, 20, 2, 1),
+        ({"oil_id": "A10K"}, 20, 0, 1),
+    ]
+)
 async def test_get_trading_result_for_period(
         trading_repository: TradingResultRepository,
         period: tuple[StartPeriodDate, EndPeriodDate],
+        filters: dict[str, str],
+        limit: int,
+        offset: int,
+        length: int
 ):
     start, end = period
     trading_result_list = await trading_repository.get_trading_result_for_period(
-        filters={},
+        filters=filters,
         start_period_date=start,
         end_period_date=end,
-        limit=20,
-        offset=0,
+        limit=limit,
+        offset=offset,
     )
-    assert len(trading_result_list) == 3
+    assert len(trading_result_list) == length
 
 
-async def test_get_trading_result_for_period_w_pagination(
+@pytest.mark.parametrize(
+    "filters, count", [
+        ({}, 3),
+        ({"oil_id": "A10K"}, 1),
+    ]
+)
+async def test_get_trading_result_count(
         trading_repository: TradingResultRepository,
-        period: tuple[StartPeriodDate, EndPeriodDate],
+        filters: dict[str, str],
+        count: int,
 ):
-    start, end = period
-    trading_result_list = await trading_repository.get_trading_result_for_period(
-        filters={},
-        start_period_date=start,
-        end_period_date=end,
-        limit=20,
-        offset=2,
-    )
-    assert len(trading_result_list) == 1
+    trading_result_count = await trading_repository.get_trading_result_count(filters=filters)
+    assert trading_result_count == count
 
 
-async def test_get_trading_result_for_period_w_filters(
+@pytest.mark.parametrize(
+    "filters, limit, offset, length", [
+        ({}, 20, 0, 3),
+        ({}, 20, 2, 1),
+        ({"oil_id": "A10K"}, 20, 0, 1),
+    ]
+)
+async def test_get_trading_result(
         trading_repository: TradingResultRepository,
-        period: tuple[StartPeriodDate, EndPeriodDate],
+        filters: dict[str, str],
+        limit: int,
+        offset: int,
+        length: int,
 ):
-    start, end = period
-    trading_result_list = await trading_repository.get_trading_result_for_period(
-        filters={"oil_id": "A10K"},
-        start_period_date=start,
-        end_period_date=end,
-        limit=20,
-        offset=0,
-    )
-    assert len(trading_result_list) == 1
-
-
-async def test_get_trading_result_count(trading_repository: TradingResultRepository):
-    trading_result_count = await trading_repository.get_trading_result_count(filters={})
-    assert trading_result_count == 3
-
-
-async def test_get_trading_result_count_w_filters(trading_repository: TradingResultRepository):
-    trading_result_count = await trading_repository.get_trading_result_count(filters={"oil_id": "A10K"})
-    assert trading_result_count == 1
-
-
-async def test_get_trading_result(trading_repository: TradingResultRepository):
     trading_result_list = await trading_repository.get_trading_result(
-        filters={},
-        limit=20,
-        offset=0,
+        filters=filters,
+        limit=limit,
+        offset=offset,
     )
-    assert len(trading_result_list) == 3
-
-
-async def test_get_trading_result_w_filters(trading_repository: TradingResultRepository):
-    trading_result_list = await trading_repository.get_trading_result(
-        filters={"oil_id": "A10K"},
-        limit=20,
-        offset=0,
-    )
-    assert len(trading_result_list) == 1
-
-
-async def test_get_trading_result_w_pagination(trading_repository: TradingResultRepository):
-    trading_result_list = await trading_repository.get_trading_result(
-        filters={},
-        limit=20,
-        offset=2,
-    )
-    assert len(trading_result_list) == 1
+    assert len(trading_result_list) == length
